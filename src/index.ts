f1e80bfbfb1c543b3998385767f4c4a088c0a1a1
ToastedToast <hey@toasted.dev>
2024-05-07 16:54:36 +0800

    fix(exports): update

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { jsml, jsml as default } from "./db";
 export { table } from "./schema/table";
-export { string, number, reference } from "./schema/types";
+export { string, number, boolean, array, reference } from "./schema/types";
 export { defineConfig } from "./bin/utils/config";
